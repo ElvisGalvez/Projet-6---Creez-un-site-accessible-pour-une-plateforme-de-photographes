20d124970b4cfe6bfb62a70d655e37781e45a0c2
ElvisGalvez <galvez.elvis@gmail.com>
2023-03-21 19:42:32 +0100

    JS en cours, début CSS

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,31 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, price, tagline } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        // Sélectionne le modèle d'article vide à cloner
+        const template = document.querySelector('#photographer-template');
+
+        // Clone le modèle
+        const article = template.content.cloneNode(true);
+
+        // Modifie le contenu de l'article cloné
+        const link = article.querySelector('.photographer-link');
+        link.setAttribute('aria-label', `Voir le profil de ${name}`);
+        const img = link.querySelector('.photographer-image');
+        img.setAttribute('src', picture);
+        img.setAttribute('alt', `photo de ${name}`);
+
+        const photographerName = link.querySelector('.photographer-name');
+        photographerName.textContent = name;
+
+        article.querySelector('.photographer-location').textContent = `${city}, ${country}`;
+        article.querySelector('.photographer-price').textContent = `${price}€/jour`;
+        article.querySelector('.photographer-tagline').textContent = tagline;
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+
+    return { getUserCardDOM };
 }
