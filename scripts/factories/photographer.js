6d19277d2155dc76913ed3f4ddc6a550520cbfd0
ElvisGalvez <galvez.elvis@gmail.com>
2023-03-27 19:57:12 +0200

    display photographer

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -6,29 +6,25 @@ function photographerFactory(data) {
     function getUserCardDOM() {
         // Sélectionne le modèle d'article vide à cloner
         const template = document.querySelector('#photographer_template');
-
+      
         // Clone le modèle
         const article = template.content.cloneNode(true);
-
+      
         // Modifie le contenu de l'article cloné
         const link = article.querySelector('.photographer_link');
-        link.setAttribute('aria-label', `Voir le profil de ${name}`);
+        link.setAttribute('aria-label', `Voir le profil de ${data.name}`);
         link.setAttribute('href', `photographer.html?id=${data.id}`);
         const img = link.querySelector('.photographer_image');
-        img.setAttribute('src', picture);
-        img.setAttribute('alt', `photo de ${name}`);
-
-        const photographerName = link.querySelector('.photographer_name');
-        photographerName.textContent = name;
-
-        article.querySelector('.photographer_location').textContent = `${city}, ${country}`;
-        article.querySelector('.photographer_price').textContent = `${price}€/jour`;
-        article.querySelector('.photographer_tagline').textContent = tagline;
-
+        img.setAttribute('src', `assets/photographers/${data.portrait}`);
+        img.setAttribute('alt', `photo de ${data.name}`);
+      
+        article.querySelector('.photographer_name').textContent = data.name;
+        article.querySelector('.photographer_location').textContent = `${data.city}, ${data.country}`;
+        article.querySelector('.photographer_price').textContent = `${data.price}€/jour`;
+        article.querySelector('.photographer_tagline').textContent = data.tagline;
+      
         return article;
-
-        
-    }
+      }
 
     return { getUserCardDOM };
 }
--- scripts/pages/photographerPage.js
+++ scripts/pages/photographerPage.js
@@ -1,29 +1,39 @@
-
-
 async function getPhotographerById(id) {
   const response = await fetch('/data/photographers.json');
   const data = await response.json();
-  const photographer = data.photographers.find(p => p.id === id);
+  const photographer = data.photographers.find(p => p.id === parseInt(id, 10));
   return photographer;
 }
 
 function getPhotographerIdFromUrl() {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get('id');
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('id');
 }
 
-
 async function init() {
-    const photographerId = getPhotographerIdFromUrl();
-  
-    if (photographerId) {
-      console.log(`Photographer ID: ${photographerId}`);
-      const photographer = await getPhotographerById(photographerId);
-      console.log(photographer);
-      // Afficher les données du photographe sur la page photographe
-    } else {
-      console.log('No photographer ID found in URL');
-    }
+  const photographerId = getPhotographerIdFromUrl();
+
+  if (photographerId) {
+    console.log(`Photographer ID: ${photographerId}`);
+    const photographer = await getPhotographerById(photographerId);
+    console.log(photographer);
+
+    const photographerInfo = document.querySelector('.photographer_info');
+
+    photographerInfo.innerHTML = `
+      <h1 class="photographer_name">${photographer.name}</h1>
+      <p class="photographer_location">${photographer.city}, ${photographer.country}</p>
+      <p class="photographer_tagline">${photographer.tagline}</p>
+    `;
+
+    const photographerPortrait = document.createElement("img");
+    photographerPortrait.className = "photographer_portrait";
+    photographerPortrait.src = `assets/photographers/${photographer.portrait}`;
+    photographerPortrait.alt = `Photo de ${photographer.name}`;
+    photographerInfo.appendChild(photographerPortrait);
+  } else {
+    console.log('No photographer ID found in URL');
   }
-  
-  init();
+}
+
+init();
